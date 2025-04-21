9a7c4a359c7700d4deacc4d29e38b5b7025d4d54
Phonkd <enst18.12@gmail.com>
2025-04-21 12:25:19 +0200

    hcange hash ig again

--- machines-nok8s/apps/reverseproxy.nix
+++ machines-nok8s/apps/reverseproxy.nix
@@ -9,7 +9,7 @@ in
   sops.secrets.cfapikey = {};
   services.caddy = {
     package = pkgs.unstable.caddy.withPlugins {
-      plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20250228175314-1fb64108d4de" ];
+      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
       hash = "sha256-pfh9DXUj35jlAntkWc4D5wuW04xxQfM1rZ4KFauMzvc=";
     };
     enable = true;
