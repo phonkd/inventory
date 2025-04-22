5cb1aaa993cf22b95f2a9c289b145bdfc79c0a53
Phonkd <enst18.12@gmail.com>
2025-04-22 21:27:23 +0200

    add new address

--- machines-nok8s/ext-mail/mail.nix
+++ machines-nok8s/ext-mail/mail.nix
@@ -27,7 +27,7 @@ in
     loginAccounts = {
       "phonkd@phonkd.net" = {
         hashedPasswordFile = hashpwtmp;
-        aliases = ["test@phonkd.net"];
+        aliases = ["test@phonkd.net" "spam@phonkd.net" "elis@phonkd.net" "info@phonkd.net"];
       };
     };
     certificateScheme = 3;
