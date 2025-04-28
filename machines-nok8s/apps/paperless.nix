a0fad2de6737f1aafd6c58f57a30e02b32db599e
Elis <elis.steiner@bedag.ch>
2025-04-28 22:48:09 +0200

    pluh

--- machines-nok8s/apps/paperless.nix
+++ machines-nok8s/apps/paperless.nix
@@ -6,8 +6,7 @@
     address= "0.0.0.0";
     settings = {
       PAPERLESS_CSRF_TRUSTED_ORIGINS = [
-        "http://localhost:28981"
-        "https://paperless.teleport.phonkd.net"
+        "http://localhost:28981, https://paperless.teleport.phonkd.net"
       ] ;
       PAPERLESS_ALLOWED_HOSTS ="paperless.teleport.phonkd.net";
       PAPERLESS_CORS_ALLOWED_ORIGINS = "https://paperless.teleport.phonkd.net";
