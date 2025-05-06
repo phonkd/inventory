bd80976ec3b0c76afcce713b223d5bbd389cf9f0
Phonkd <enst18.12@gmail.com>
2025-05-06 16:54:24 +0200

    add mc

--- machines-nok8s/apps/mc.nix
+++ machines-nok8s/apps/mc.nix
@@ -1,17 +1,21 @@
 { config, pkgs, lib, ... }:
 {
-  services.minecraft-server.declarative = true;
-  minecraft-server.serverProperties = {
-    levelName = "world";
-    maxPlayers = 20;
-    motd = "Nu uh";
-    difficulty = "Hard";
-    gamemode = "survival";
-    white-list = true;
-    enable-rcon = true;
-    "rcon.password" = "hunter2";
+  services.minecraft-server = {
+    declarative = true;
+    eula = true;
+    openFirewall = true;
+    jvmOpts = "-Xmx16384M -Xms2048M";
+    serverProperties = {
+      levelName = "world";
+      maxPlayers = 20;
+      motd = "Nu uh";
+      difficulty = "Hard";
+      gamemode = "survival";
+      white-list = true;
+      enable-rcon = true;
+      "rcon.password" = "hunter2";
+    };
   };
-  services.minecraft-server.eula = true;
-  services.minecraft-server.openFirewall = true;
-  services.minecraft-server.jvmOpts = "-Xmx16384M -Xms2048M";
+
+
 }
