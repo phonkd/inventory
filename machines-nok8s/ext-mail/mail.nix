9b1b00c982d5fdd07f2f79b9f38a18b10de924f9
Elis <elis.steiner@bedag.ch>
2025-06-10 15:19:10 +0200

    cal

--- machines-nok8s/ext-mail/mail.nix
+++ machines-nok8s/ext-mail/mail.nix
@@ -9,6 +9,12 @@ let
                     config.sops.secrets."sofia".path
                   else
                     "/dev/null";
+  mailAccounts = config.mailserver.loginAccounts;
+  htpasswd = pkgs.writeText "radicale.users" (concatStrings
+    (flip mapAttrsToList mailAccounts (mail: user:
+      mail + ":" + user.hashedPassword + "\n"
+    ))
+  );
 in
 {
   sops.secrets."mail-secret" = {
@@ -45,4 +51,35 @@ in
   };
   security.acme.acceptTerms = true;
   security.acme.defaults.email = "bhonk123@gmail.com";
+
+  # calendar config:
+  services.radicale = {
+    enable = true;
+    settings = {
+      auth = {
+        type = "htpasswd";
+        htpasswd_filename = "${htpasswd}";
+        htpasswd_encryption = "bcrypt";
+      };
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts = {
+      "cal.phonkd.net" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://localhost:5232/";
+          extraConfig = ''
+            proxy_set_header  X-Script-Name /;
+            proxy_set_header  X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_pass_header Authorization;
+          '';
+        };
+      };
+    };
+  };
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
