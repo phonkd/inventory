8a6658d826c4c71aec66c5be3229aba6a9d91ec0
Elis <elis.steiner@bedag.ch>
2025-06-12 18:14:42 +0200

    wtf

--- machines-nok8s/122-nix-int/flake.nix
+++ machines-nok8s/122-nix-int/flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
   };
   outputs = { self, nixpkgs, nixpkgs-unstable, sops-nix }:
