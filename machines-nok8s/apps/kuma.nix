7d8bd3de9046213b84a45f7470d3d5f3a931b6d5
Elis <elis.steiner@bedag.ch>
2025-06-16 23:16:13 +0200

    kuma

--- machines-nok8s/apps/kuma.nix
+++ machines-nok8s/apps/kuma.nix
@@ -1,4 +1,5 @@
-{ config, pkgs, lib }:
+# Auto-generated using compose2nix v0.3.1.
+{ config, pkgs, lib, ... }:
 {
   services.uptime-kuma.enable = true;
   services.teleport.settings = {
@@ -8,6 +9,23 @@
         {
           name = "kuma";
           uri = "http://localhost:4000";
+        }
+      ];
+    };
+  };
+  services.teleport.settings = {
+    app_service = {
+      enabled = true;
+      apps = [
+        {
+          name = "kuma";
+          uri = "http://localhost:4000";
+          # insecure_skip_verify = true;
+          # rewrite = {
+          #   headers = [
+          #     "Host: paperless.teleport.phonkd.net"
+          #   ];
+          # };
         }
       ];
     };
