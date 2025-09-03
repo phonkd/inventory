45a53feb3d1033da77ab7e6c0d785c0276d6b970
Elis <elis.steiner@bedag.ch>
2025-09-03 11:08:26 +0200

    fix mail

--- modules/ext-mail/mail.nix
+++ modules/ext-mail/mail.nix
@@ -21,7 +21,7 @@ in
       url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/nixos-25.05/nixos-mailserver-nixos-25.05.tar.gz";
       # To get the sha256 of the nixos-mailserver tarball, we can use the nix-prefetch-url command:
       # release="nixos-23.05"; nix-prefetch-url "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/${release}/nixos-mailserver-${release}.tar.gz" --unpack
-      sha256 = "0jpp086m839dz6xh6kw5r8iq0cm4nd691zixzy6z11c4z2vf8v85";
+      sha256 = "sha256:1qn5fg0h62r82q7xw54ib9wcpflakix2db2mahbicx540562la1y";
     })
   ];
   mailserver = {
