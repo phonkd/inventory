ff41146ed66e8aa1df57876fae73a0dddbfbb740
Phonkd <enst18.12@gmail.com>
2025-10-13 14:14:25 +0200

    idk

--- machines/121-nix-services/ssh.nix
+++ machines/121-nix-services/ssh.nix
@@ -16,6 +16,9 @@
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDg0PjpVeFevKuUq7ZVAhL0fySgOomRT/SZ6jWFxfv0q06KgwLSInwXFZDIUNN9c2Uz6qgJvh/xZ9UQfuoYwBMwUDt89hhplZDeFG+0kTxPRyjKrtcOXefM2ne4eI93kvJfU5+SaxXs3GF5oChoml4Wwub74CVLWIlKTvA7YLEKzBffEJ4ypO97YTR734Cd1vHsIOVFylftIpe0n/oA7o3Bu+GSRwfW4cM9nbYcumydwyrA9osrQ6dLNFCJ6DSvBY65j9eU/wGEObmch645f+hAm1ROZxoUYtVBQjSNheYNIUAxjXDbHd/eA3TjG6qGfUSbFu1gitQBLY4M+YUmT+r/IjD3XBFwFCED3G/TKKBjKubCMk0yxegCa+JZt+HzSbRTILgFv0eC+DvZBgMHMx0RjefvOJY6mCWtwwYRULp+2ulls6RTX2F3aEEKO0+/9YxTfzvwE1zFLAVxNpCg25f35eWuBdIJD/2K42Krbe2xrGDJdFhRtpT1uoq0qGHreIk= phonkd@Eliss-MacBook-Pro.local"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINlfQKdDHjPToqS5Ui0oAGAdgbh7B+NICMP7eYMrrska phonkd@nixos"
   ];
+  nix.settings.trusted-public-keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINlfQKdDHjPToqS5Ui0oAGAdgbh7B+NICMP7eYMrrska phonkd@nixos"
+  ];
   users.users.phonkd = {
     isNormalUser = true;
     description = "phonkd";
