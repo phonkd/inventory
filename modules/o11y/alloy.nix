f153e586c4b739c50d4b14e2256d6f8361e0f34d
Phonkd <enst18.12@gmail.com>
2025-10-26 14:59:00 +0100

    Update alloy.nix

--- modules/o11y/alloy.nix
+++ modules/o11y/alloy.nix
@@ -14,7 +14,8 @@
     text = ''
       prometheus.scrape "nixvms" {
         targets    = [ {
-          "__address__" = "127.0.0.1:9100/metrics",
+          "__address__" = "127.0.0.1:9100"
+          "__metrics_path__" = "/metrics"
         } ]
         forward_to = [prometheus.remote_write.nixvms.receiver]
       }
