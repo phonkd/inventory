942b3653f3b9cf4797e2c9f723eebfd1694aa211
phonkd <enst18.12@gmail.com>
2025-12-02 22:08:12 +0100

    update key for mail
    Signed-off-by: phonkd <enst18.12@gmail.com>

--- modules/ext-mail/configuration.nix
+++ modules/ext-mail/configuration.nix
@@ -1,4 +1,5 @@
-{ lib, ... }: {
+{ lib, ... }:
+{
   imports = [
     /etc/nixos/configuration.nix
     ./sops.nix
@@ -6,8 +7,7 @@
   ];
   users.users."phonkd".openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDg0PjpVeFevKuUq7ZVAhL0fySgOomRT/SZ6jWFxfv0q06KgwLSInwXFZDIUNN9c2Uz6qgJvh/xZ9UQfuoYwBMwUDt89hhplZDeFG+0kTxPRyjKrtcOXefM2ne4eI93kvJfU5+SaxXs3GF5oChoml4Wwub74CVLWIlKTvA7YLEKzBffEJ4ypO97YTR734Cd1vHsIOVFylftIpe0n/oA7o3Bu+GSRwfW4cM9nbYcumydwyrA9osrQ6dLNFCJ6DSvBY65j9eU/wGEObmch645f+hAm1ROZxoUYtVBQjSNheYNIUAxjXDbHd/eA3TjG6qGfUSbFu1gitQBLY4M+YUmT+r/IjD3XBFwFCED3G/TKKBjKubCMk0yxegCa+JZt+HzSbRTILgFv0eC+DvZBgMHMx0RjefvOJY6mCWtwwYRULp+2ulls6RTX2F3aEEKO0+/9YxTfzvwE1zFLAVxNpCg25f35eWuBdIJD/2K42Krbe2xrGDJdFhRtpT1uoq0qGHreIk= phonkd@Eliss-MacBook-Pro.local"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJxyCQHxLqEsSRPGG5SeN4MsOGoc9mrrEioU2mQ9Xz4z phonkd@nixos"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILJdVk07A/nmTr3I+GZx9vIU5RYPl1eimdJ2Vw6oslFr phonkd@nixos"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFrYSWQbmJ2oL4nORm6U0qiJAmrgE2dNQVKlV36i5uiF phonkd@blac"
     # content of authorized_keys file
     # note: ssh-copy-id will add user@your-machine after the public key
     # but we can remove the "@your-machine" part
@@ -22,7 +22,11 @@
   users.users.phonkd = {
     isNormalUser = true;
     description = "phonkd";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "docker"
+    ];
   };
   security.sudo.wheelNeedsPassword = false;
   sops.age.keyFile = /root/.config/sops/age/keys.txt;
