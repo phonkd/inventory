b438ec3f0689d15b9b9beff77856307b957f5f61
Elis <elis.steiner@bedag.ch>
2026-01-23 22:09:04 +0100

    sad

--- modules/dns-darwin.nix
+++ modules/dns-darwin.nix
@@ -4,7 +4,7 @@
   services.dnsmasq = {
     enable = true;
     bind = "127.0.0.1";
-    
+
     # Upstream DNS servers (Cloudflare)
     servers = [
       "1.1.1.1"
@@ -14,6 +14,7 @@
     # Local domain resolution
     addresses = {
       ".int.phonkd.net" = "192.168.1.201";
+      ".w.int.phonkd.net" = "192.168.1.201";
       ".segglaecloud.phonkd.net" = "192.168.1.123";
       ".w.phonkd.net" = "192.168.1.201";
     };
