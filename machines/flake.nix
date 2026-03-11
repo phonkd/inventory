b3656eb8123178a6d1fbe524eccc67a9721aeda1
Elis <elis.steiner@bedag.ch>
2026-03-11 05:19:05 +0100

    remove

--- machines/flake.nix
+++ machines/flake.nix
@@ -16,10 +16,10 @@
       # Optional but recommended to limit the size of your system closure.
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    work-setup = {
-      url = "git+ssh://git@github.com/phonkd/bedag-setup";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # work-setup = {
+    #   url = "git+ssh://git@github.com/phonkd/bedag-setup";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
     ambxst = {
       url = "git+https://github.com/Axenide/Ambxst/";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
