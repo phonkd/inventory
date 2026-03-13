53c994415c56d019f6326a25a793ffdebf43eaeb
Phonkd <phonkd@phonkd.net>
2026-03-13 17:22:09 +0100

    kubecolor

--- modules/home/terminal.nix
+++ modules/home/terminal.nix
@@ -5,10 +5,10 @@
     enable = true;
     package = pkgs.ghostty-bin;
     settings = {
-      theme = "Adventure";
+      theme = "Dracula";
       font-size = 16;
       confirm-close-surface = false;
-      keybind = [ "super+enter=new_split:right" ];
+      keybind = [ "super+enter=new_split:auto" ];
     };
   };
   programs.zsh = {
@@ -22,52 +22,38 @@
         cat "$1" | pbcopy
       '';
     };
+    autosuggestion.enable = true;
+  };
+  programs.fish = {
+    enable = true;
   };
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+    enableFishIntegration = true;
+    presets = [ "nerd-font-symbols" ];
     settings = {
-      format = ''
-        [░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$kubernetes[](fg:#212736 bg:#1d2230)$time[ ](fg:#1d2230)
-        $character
-      '';
-      directory = {
-        style = "fg:#e3e5e5 bg:#769ff0";
-        format = "[ $path ]($style)";
-        truncation_length = 3;
-        truncation_symbol = ".../";
-      };
-      git_branch = {
-        symbol = "";
-        style = "bg:#394260";
-        format = "[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)";
-      };
-      git_status = {
-        style = "bg:#394260";
-        format = "[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)";
-      };
       kubernetes = {
         disabled = false;
-        style = "bg:#212736";
-        format = "[[ ☸ $context( \\($namespace\\)) ](fg:#769ff0 bg:#212736)]($style)";
-      };
-      time = {
-        disabled = false;
-        time_format = "%R";
-        style = "bg:#1d2230";
-        format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
       };
     };
   };
   programs.zoxide = {
     enable = true;
     enableZshIntegration = true;
+    enableFishIntegration = true;
   };
   programs.kubecolor = {
     enable = true;
     enableZshIntegration = true;
+    #enableAlias = true;
   };
   home.packages = with pkgs; [
     nerd-fonts.symbols-only
   ];
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+  };
 }
