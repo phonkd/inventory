301dbc78f4787ac66989095b49a3bdba2d0f8d50
Phonkd <phonkd@phonkd.net>
2026-03-15 18:42:49 +0100

    automate backup of existing hm files

--- machines/flake.nix
+++ machines/flake.nix
@@ -153,6 +153,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "bak";
               home-manager.users.phonkd = import ./g14/home.nix;
             }
             (
